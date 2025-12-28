59611e79628cc361d7836101be66eba6fa056cbf
lagtrain <argoevchik@gmail.com>
2025-12-28 03:10:34 +0200

    refactor

--- archlinux-saga.sh
+++ archlinux-saga.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# echo "Downoading solution"
+echo "THE PROGRAM IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION."
+read -p "Press [Enter] key to continue..." hwX
+
+# Downoading solution
 # git clone https://github.com/nadimkobeissi/16iax10h-linux-sound-saga.git
-#
+
 setopt() {
 # look for/create parametr in file and assigns value to it
 # syntax setopt <file> <parametr> <value>
@@ -18,25 +21,42 @@ setopt() {
   echo "$key=$val option set"
 }
 
-echo "copy firmware"
-cp -f fix/firmware/aw88399_acf.bin /lib/firmware/aw88399_acf.bin
-read -p "Press [Enter] key to continue..." tmp
-
 current_kernel_version="linux-$(sed 's+-.*++g' <<< "$(uname -r)")"
 current_kernel_tarball="$current_kernel_version.tar.xz"
 patch_name="16iax10h-audio-$current_kernel_version.patch"
 
-echo "$current_kernel_tarball"
+if [ -s "fix/patches/$patch_name" ] ; then
+  echo "patch version match"
+else
+echo "Kernel: $current_kernel_version"
+echo "patch_name: $patch_name"
+echo "tarball: $current_kernel_tarball"
+echo ""
+echo "Exact patch match was not found"
+echo "Press [Ctrl+C] key to exit"
+read -p "Press [Enter] key to continue..." tmp
+  patch_name="16iax10h-audio-6.18.patch"
+  echo "fallback to patch name:"
+  echo "$patch_name"
+fi
+echo "Kernel: $current_kernel_version"
+echo "patch_name: $patch_name"
+echo "tarball: $current_kernel_tarball"
 read -p "Press [Enter] key to continue..." tmp
 
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/$current_kernel_tarball
-if [ -s $current_kernel_tarball]; then
+echo "copy firmware"
+cp -f fix/firmware/aw88399_acf.bin /lib/firmware/aw88399_acf.bin
+read -p "Press [Enter] key to continue..." tmp
+
+
+if [ -s $current_kernel_tarball ]; then
   rm $current_kernel_tarball
 fi
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/$current_kernel_tarball
 
 echo "linux kernel downloaded"
 
-if [ -s $current_kernel_version]; then
+if [ -s $current_kernel_version ]; then
   rm $current_kernel_version
 fi
 
@@ -70,6 +90,13 @@ sudo make -j24 modules_install
 echo "look if kernel compiled"
 read -p "Press [Enter] key to continue..." tmp
 sudo cp -f arch/x86/boot/bzImage /boot/vmlinuz-linux-16iax10h-audio
+
+sudo cp /etc/mkinitcpio.d/linux.preset /etc/mkinitcpio.d/linux-16iax10h-audio.preset
+
+mkinitcpio_preset=/etc/mkinitcpio.d/linux-16iax10h-audio.preset
+setopt $mkinitcpio_preset ALL_kver ""/boot/vmlinuz-linux-16iax10h-audio""
+setopt $mkinitcpio_preset PRESETS "('default')"
+setopt $mkinitcpio_preset default_image ""/boot/initramfs-linux-16iax10h-audio.img""
 
 # NOTE:
 # sudo limine-entry-tool --add new-linux-16iax10h-audio /boot/cryptichashshit/linux-16iax10h-audio/omarchy_linux.efi /boot/cryptichashshit/linux-16iax10h-audio/vmlinuz-linux-16iax10h-audio "" --comment "Linux for Lenovo Legion 7 16iax10h with Audio Support"
--- postfix.sh
+++ postfix.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 echo "THE PROGRAM IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION."
 read -p "Press [Enter] key to continue..." hwX
 
-touch /etc/modprobe.d/inteldsp.conf
-setopt /etc/modprobe.d/inteldsp.conf snd_intel_dspcfg.dsp_driver 3
+# touch /etc/modprobe.d/inteldsp.conf
+# setopt /etc/modprobe.d/inteldsp.conf snd_intel_dspcfg.dsp_driver 3
 
 cp -f fix/ucm2/HiFi-analog.conf /usr/share/alsa/ucm2/HDA/HiFi-analog.conf
 cp -f fix/ucm2/HiFi-mic.conf /usr/share/alsa/ucm2/HDA/HiFi-mic.conf
